fc191bf5c54b1ac5ea28493c11fb8f1c28857ad2
selenium28 <devselenium.class2819@gmail.com>
2025-06-29 10:55:10 +0530

    Java Basic Programs Started

---
+++ src/basic/DataTypes.java
@@ -0,0 +1,47 @@
+package basic;
+
+public class DataTypes { // class body
+
+	public static void main(String[] args) { // main method body
+//		Primitive Data types: int, double, char, boolean (Size is fixed)
+//		Non-Primitive Data types: String, Array etc. (Size is not fixed)
+//		1. Int
+		int a = 10;
+//		int a = 20; Duplicate variable not allowed in Java
+		System.out.println(a);
+		int a1; 
+		a1 = 20;
+		int a2 = -5;
+		int mob = 98765443;
+		System.out.println(mob);
+		
+//		2. Double
+		double d = 33.33;
+		System.out.println(d);
+		double d1 = 1000;
+		System.out.println(d1);
+		
+//		3. Char
+		char c = 'A';
+		char c1 = '@';
+		System.out.println(c);
+		
+//		4. Boolean
+//		What is true & false in Java? True & False are keywords in java 
+//		but we can use as values
+		boolean b = true;
+		boolean b1 = false;
+		System.out.println(b);
+		
+//		5. String
+//		String is a Class / String is a sequence of char.
+		String s = "Dev";
+		String s1 = "Java Selenium";
+		System.out.println(s1);
+		String s2 = "dev123$#@test.com";
+		System.out.println(s2);
+		
+		
+	}
+
+}
